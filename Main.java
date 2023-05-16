4ec33a2de0082c9a824a87eeff835bf85e451861
masamasakoikoi <koichan1996@icloud.com>
2023-05-16 13:41:24 +0000

    1-4

--- Main.java
+++ Main.java
@@ -1,13 +1,9 @@
 public class Main {
-    public static void main(String[] args) {
-        String s1 = "Java and Javascript";
-        if(s1.contains("Java")) {
-            System.out.println("文字列s1は、Javaを含んでいます");
-        }
-        if(s1.endsWith("Java")) {
-            System.out.println("文字列s1はJavaが末尾にあります");
-        }
-        System.out.println("文字列s1で最初にJavaが登場する位置は" + s1.indexOf("Java"));
-        System.out.println("文字列s1で最後にJavaが登場する位置は" + s1.lastIndexOf("Java"));
-        }
+    public static void main (String[] args) {
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < 10000; i++){
+       sb.append("Java");
     }
+    String s = sb.toString();
+    }
+}
