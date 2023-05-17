1ed0d7e6aaf8ee836d2b8fe165fa16f4637fac66
masamasakoikoi <koichan1996@icloud.com>
2023-05-17 14:40:52 +0000

    1-5

--- Main.java
+++ Main.java
@@ -1,9 +1,16 @@
-public class Main {
-    public static void main (String[] args) {
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < 10000; i++){
-       sb.append("Java");
+boolean isValidPlayerName(String name) {
+    if(name.length() != 8){
+        return false;
+
+    char first = name.charAt(0);
+    if(!(first >= 'A' && first <= 'Z')){
+        return false;
     }
-    String s = sb.toString();
+    for(int i = 1; i < 8; i++){
+        char c = name.charAt(i);
+        if(!((c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9'))){
+            return false;
+        }
     }
+    return true;
 }
