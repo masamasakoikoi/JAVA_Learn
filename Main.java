439ba37142f699f868e9c178edc16d946c8ae063
masamasakoikoi <koichan1996@icloud.com>
2023-05-23 15:11:16 +0000

    1-7

--- Main.java
+++ Main.java
@@ -1,17 +1,7 @@
-    boolean isValidPlayerName(String name) {
-        if(name.length() != 8) {                /* 文字数が8文字であること */
-            return false;
-        
-        char first = name.charAt(0);
-        if(!(first >= 'A' && first <= 'Z')) {   /* 最初の1文字はA～Z */
-            return false;
-        }
-        for(int i = 1; i < 8; i++) {
-            char c = name.charAt(i);
-            if(!((c >= 'A' && c <= 'Z') || (c >= '0' && first <= '9'))) {
-                return false;                   /* 以降の文字はA～Zか0～9 */
-            }
-        }
-        return true;
+public class Main {
+    public static void main(String[] args) {
+        String s = "abc,def:ghi";
+        String[] words = s.split("[,:]");   /* 正規表現パターン */
+        for(String w : words) { System.out.println(w + "->"); }
     }
-
+}
